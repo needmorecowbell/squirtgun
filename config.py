d2328c2f904643c5bb7173f88ee95f22188234c0
root <root@localhost.localdomain>
2018-06-25 14:31:45 -0400

    First commit

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ digital_ocean = {
         'token':'',
         'ssh-key':'keys/digitaloceanKey.pub',
         'ssh-key-priv':'keys/digitaloceanKey',
-        'installer-url':'https://raw.githubusercontent.com/needmorecowbell/jumper/master/master/installMaster.sh',
+        'installer-url':'url-here/installMaster.sh',
         }
 
